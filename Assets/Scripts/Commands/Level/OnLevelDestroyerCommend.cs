a58d6258232310b3268e0b6ecb6bf949af417fa1
MuhammetMelihKIR <muhammetmelihkr@gmail.com>
2023-11-01 14:36:51 +0300

    addLevelManager

---
+++ Assets/Scripts/Commands/Level/OnLevelDestroyerCommend.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+namespace Commands.Level
+{
+    public class OnLevelDestroyerCommend
+    {
+        private Transform _levelHolder;
+        public OnLevelDestroyerCommend(Transform levelHolder)
+        {
+           _levelHolder = levelHolder;
+        }
+
+        public void Execute()
+        {
+            if (_levelHolder.transform.childCount <=0 )return;
+            Object.Destroy(_levelHolder.transform.GetChild(0).gameObject);
+           
+        }
+    }
+}
