2fb68a373916bb7d0c929f2e654d8e68289f6298
MuhammetMelihKIR <muhammetmelihkr@gmail.com>
2023-11-03 12:40:43 +0300

    add InputManager

--- Assets/Scripts/Commands/Level/OnLevelDestroyerCommend.cs
+++ Assets/Scripts/Commands/Level/OnLevelDestroyerCommend.cs
@@ -5,12 +5,12 @@ namespace Commands.Level
     public class OnLevelDestroyerCommend
     {
         private Transform _levelHolder;
-        public OnLevelDestroyerCommend(Transform levelHolder)
+        internal OnLevelDestroyerCommend(Transform levelHolder)
         {
            _levelHolder = levelHolder;
         }
 
-        public void Execute()
+        internal void Execute()
         {
             if (_levelHolder.transform.childCount <=0 )return;
             Object.Destroy(_levelHolder.transform.GetChild(0).gameObject);
